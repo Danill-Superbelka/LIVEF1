fd0be6238362a9e7d0f7229abc72ef2e64c0bb18
Даниил <d.belevich@mangazeya.ru>
2024-08-06 17:16:35 +0300

    Init project

---
+++ LiveF/LiveFApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveFApp.swift
+//  LiveF
+//
+//  Created by Даниил  on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LiveFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
