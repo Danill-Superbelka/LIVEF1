46c59a5dfb7463231bb95cd9921444e7355788cc
Даниил <d.belevich@mangazeya.ru>
2024-08-06 18:09:43 +0300

    Add SwiftLint, SwiftFormat

--- LiveFTests/LiveFTests.swift
+++ LiveFTests/LiveFTests.swift
@@ -5,11 +5,10 @@
 //  Created by Даниил  on 06.08.2024.
 //
 
-import XCTest
 @testable import LiveF
+import XCTest
 
 final class LiveFTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class LiveFTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- LiveFUITests/LiveFUITestsLaunchTests.swift
+++ LiveFUITests/LiveFUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class LiveFUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
